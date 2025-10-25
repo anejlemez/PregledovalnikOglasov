a4f4429c9c94846d7c7c6731573fa0f8371b4cc6
anejlemez <anejgti.lemez@gmail.com>
2025-10-25 20:59:14 +0200

    Delujoče slike

--- PregledovalnikOglasov/MainWindow.xaml.cs
+++ PregledovalnikOglasov/MainWindow.xaml.cs
@@ -29,13 +29,13 @@ namespace PregledovalnikOglasov
         {
             InitializeComponent();
 
-            _oglasi.Add(new Oglas { Naziv = "Audi A4 2.0 TDI", Cena = 12900, Letnik = 2018, Km = 145000, Gorivo = "Dizel", ImagePath = "Images/audi.jpg" });
-            _oglasi.Add(new Oglas { Naziv = "BMW 320d xDrive", Cena = 15900, Letnik = 2019, Km = 120000, Gorivo = "Dizel", ImagePath = "Images/bmw.jpg" });
-            _oglasi.Add(new Oglas { Naziv = "Volkswagen Golf 1.6 TDI", Cena = 8900, Letnik = 2016, Km = 175000, Gorivo = "Dizel", ImagePath = "Images/golf.jpg" });
-            _oglasi.Add(new Oglas { Naziv = "Renault Clio 1.2 16V", Cena = 6900, Letnik = 2015, Km = 98000, Gorivo = "Bencin", ImagePath = "Images/clio.jpg" });
-            _oglasi.Add(new Oglas { Naziv = "Škoda Octavia 2.0 TDI", Cena = 11900, Letnik = 2017, Km = 130000, Gorivo = "Dizel", ImagePath = "Images/octavia.jpg" });
-            _oglasi.Add(new Oglas { Naziv = "Peugeot 308 1.2 PureTech", Cena = 7500, Letnik = 2016, Km = 160000, Gorivo = "Bencin", ImagePath = "Images/peugeot.jpg" });
-            _oglasi.Add(new Oglas { Naziv = "Toyota Yaris Hybrid", Cena = 10400, Letnik = 2018, Km = 90000, Gorivo = "Hibrid", ImagePath = "Images/yaris.jpg" });
+            _oglasi.Add(new Oglas { Naziv = "Audi A4 2.0 TDI", Cena = 12900, Letnik = 2018, Km = 145000, Gorivo = "Dizel", ImagePath = "https://assets.adac.de/image/upload/v1/Autodatenbank/Fahrzeugbilder/im05628-1-audi-a4.jpg" });
+            _oglasi.Add(new Oglas { Naziv = "BMW 320d xDrive", Cena = 15900, Letnik = 2019, Km = 120000, Gorivo = "Dizel", ImagePath = "https://www.avtoo.si/assets/img/car/1189/02_718825536.jpg" });
+            _oglasi.Add(new Oglas { Naziv = "Volkswagen Golf 1.6 TDI", Cena = 8900, Letnik = 2016, Km = 175000, Gorivo = "Dizel", ImagePath = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/2020_Volkswagen_Golf_Style_1.5_Front.jpg/1200px-2020_Volkswagen_Golf_Style_1.5_Front.jpg" });
+            _oglasi.Add(new Oglas { Naziv = "Renault Clio 1.2 16V", Cena = 6900, Letnik = 2015, Km = 98000, Gorivo = "Bencin", ImagePath = "https://upload.wikimedia.org/wikipedia/commons/8/88/Nouvelle_Renault_Clio_E-Tech_full_hybrid.png" });
+            _oglasi.Add(new Oglas { Naziv = "Škoda Octavia 2.0 TDI", Cena = 11900, Letnik = 2017, Km = 130000, Gorivo = "Dizel", ImagePath = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTowW4zgOIwrKqqXnAZ84x87oO9NK7C98SpTw&s" });
+            _oglasi.Add(new Oglas { Naziv = "Peugeot 308 1.2 PureTech", Cena = 7500, Letnik = 2016, Km = 160000, Gorivo = "Bencin", ImagePath = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/2022_-_Peugeot_308_III_%28C%29_-_121.jpg/1200px-2022_-_Peugeot_308_III_%28C%29_-_121.jpg" });
+            _oglasi.Add(new Oglas { Naziv = "Toyota Yaris Hybrid", Cena = 10400, Letnik = 2018, Km = 90000, Gorivo = "Hibrid", ImagePath = "https://www.motorfinity.uk/photos/Yaris_Blog_1732619536.jpg" });
 
             // bind to ListView
             lvOglasi.ItemsSource = _oglasi;
